858d47494a51e479ba81b5b4713a9cdb5a13dbe7
Kan8Udon <111727291+Kan8Udon@users.noreply.github.com>
2022-09-01 00:07:33 +0700

    Add files via upload

---
+++ Pad.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldPhonePadSharp
+{
+    class Pad
+    {       
+        public static string OldPhonePad(string input)
+        {
+            string oLetter = "";
+
+            if (input == "1")
+            {
+                oLetter = "&";
+            }
+            else if (input == "11")
+            {
+                oLetter = "'";
+            }
+            else if (input == "111")
+            {
+                oLetter = "(";
+            }
+            else if (input == "2")
+            {
+                oLetter = "A";
+            }
+            else if (input == "22")
+            {
+                oLetter = "B";
+            }
+            else if (input == "222")
+            {
+                oLetter = "C";
+            }
+            else if (input == "3")
+            {
+                oLetter = "D";
+            }
+            else if (input == "33")
+            {
+                oLetter = "E";
+            }
+            else if (input == "333")
+            {
+                oLetter = "F";
+            }
+            else if (input == "4")
+            {
+                oLetter = "G";
+            }
+            else if (input == "44")
+            {
+                oLetter = "H";
+            }
+            else if (input == "444")
+            {
+                oLetter = "I";
+            }
+            else if (input == "5")
+            {
+                oLetter = "J";
+            }
+            else if (input == "55")
+            {
+                oLetter = "K";
+            }
+            else if (input == "555")
+            {
+                oLetter = "L";
+            }
+            else if (input == "6")
+            {
+                oLetter = "M";
+            }
+            else if (input == "66")
+            {
+                oLetter = "N";
+            }
+            else if (input == "666")
+            {
+                oLetter = "O";
+            }
+            else if (input == "7")
+            {
+                oLetter = "P";
+            }
+            else if (input == "77")
+            {
+                oLetter = "Q";
+            }
+            else if (input == "777")
+            {
+                oLetter = "R";
+            }
+            else if (input == "7777")
+            {
+                oLetter = "S";
+            }
+            else if (input == "8")
+            {
+                oLetter = "T";
+            }
+            else if (input == "88")
+            {
+                oLetter = "U";
+            }
+            else if (input == "888")
+            {
+                oLetter = "V";
+            }
+            else if (input == "9")
+            {
+                oLetter = "W";
+            }
+            else if (input == "99")
+            {
+                oLetter = "X";
+            }
+            else if (input == "999")
+            {
+                oLetter = "Y";
+            }
+            else if (input == "9999")
+            {
+                oLetter = "Z";
+            }
+            else if (input == "0")
+            {
+                oLetter = " ";
+            }
+            /*
+            else
+            {
+                oLetter = "Invalid combination";
+            }
+            */
+            return oLetter;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldPhonePadSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pressNum;
+            
+            Console.WriteLine("Press pad combination");
+            pressNum = Console.ReadLine();
+
+
+
+
+            //Pad OldPhonePad(pressNum); 
+            Console.WriteLine(typePad(pressNum));
+            Console.ReadLine();
+        }
+
+        public static string typePad(string input)
+        {
+            string oLetter, output = "";                                     // store character
+            var oNum1 = "";
+            var oNum2 = "";
+            // string oNum2, output = "";
+
+
+            for (int i = 0; i < input.Length; i++)              // Loop to insert letter
+            {
+                if (input[i] == '*')                            // check * to delete charater
+                {
+                    output = output.Substring(0, output.Length - 1);
+                }
+                else if (input[i] == '#')                       // check # to send
+                {
+                    break;
+                }
+                else if (input[i] == input[i + 1])              // store iteration number
+                {
+                    oNum1 = oNum1 + input[i];
+                }
+                else if (input[i] != input[i + 1] || input[i] == ' ')              // store letter when iteration change
+                {
+                    oNum2 = oNum1 + input[i];
+                    oLetter = Pad.OldPhonePad(oNum2);
+                    oNum1 = string.Empty;                        // reset for new iteration
+                    output = output + oLetter;                  // store letters
+                }
+
+
+
+            }
+
+            return output;
+        }
+    }
+}
